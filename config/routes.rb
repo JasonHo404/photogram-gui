4a0da50531eac015775b79b1cc06df3b48c9c3dd
jason ho <43012595+JasonHo404@users.noreply.github.com>
2023-10-03 21:42:20 +0000

    added everything, Final

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   get("/photos/:photo_id", {controller: "photos", action: "show"})
   get("/delete_photo/:photo_id", {controller: "photos", action: "destroy"})
   post("/insert_photo_record", {controller: "photos", action: "post_photo"})
+  post("/update_photo/:photo_id", {controller: "photos", action: "update_photo"})
+  post("/add_comment", {controller: "photos", action: "add_comment"})
 end
